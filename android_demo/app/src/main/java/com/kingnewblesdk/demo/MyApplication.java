4320c164301830c807996382943b1c28983d868c
hekang <277713365@qq.com>
2017-10-20 16:46:06 +0800

    更新SDK为3.8 修改API文档以及readMe文件

--- android_demo/app/src/main/java/com/kingnewblesdk/demo/MyApplication.java
+++ android_demo/app/src/main/java/com/kingnewblesdk/demo/MyApplication.java
@@ -21,8 +21,8 @@ public class MyApplication extends Application {
         QNLog.DEBUG = true;
         QNLog.log("application启动");
 
-        //123456789是测试版的appid，第二个Boolean参数为true则是发布模式的SDK，false则是开发版的
-        QNApiManager.getApi(getApplicationContext()).initSDK("123456789", false, new QNResultCallback() {
+        //123456789是测试版的appid
+        QNApiManager.getApi(getApplicationContext()).initSDK("123456789", new QNResultCallback() {
             @Override
             public void onCompete(int errorCode) {
                 Log.i("hdr", "执行结果校验:" + errorCode);
