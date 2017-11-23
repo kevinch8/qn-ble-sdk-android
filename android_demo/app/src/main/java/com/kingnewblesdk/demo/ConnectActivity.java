9a33ecb01ad59091b29c16bf5547710b7113fc30
hekang <277713365@qq.com>
2017-11-23 15:02:41 +0800

    增加测量过程中修改用户资料的方法

--- android_demo/app/src/main/java/com/kingnewblesdk/demo/ConnectActivity.java
+++ android_demo/app/src/main/java/com/kingnewblesdk/demo/ConnectActivity.java
@@ -9,11 +9,21 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hdr.yolanda.kingnewblesdk.app.R;
-import com.kitnew.ble.*;
+import com.kitnew.ble.QNApiManager;
+import com.kitnew.ble.QNBleApi;
+import com.kitnew.ble.QNBleCallback;
+import com.kitnew.ble.QNBleDevice;
+import com.kitnew.ble.QNData;
+import com.kitnew.ble.QNUser;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,6 +34,11 @@ public class ConnectActivity extends AppCompatActivity implements QNBleCallback 
     TextView nameTv;
     TextView macTv;
     Button connectBtn;
+
+    RadioGroup sexRg;
+    EditText idEt;
+    EditText heightEt;
+    EditText birthdayEt;
 
     TextView weightTv;
     TextView modelTv;
@@ -50,6 +65,8 @@ public class ConnectActivity extends AppCompatActivity implements QNBleCallback 
         initViews();
 
         initData();
+
+        resetUserView();
     }
 
     void initViews() {
@@ -61,11 +78,24 @@ public class ConnectActivity extends AppCompatActivity implements QNBleCallback 
         modelTv = (TextView) findViewById(R.id.modelTv);
         statusTv = (TextView) findViewById(R.id.statusTv);
 
+        idEt = (EditText) findViewById(R.id.idEt);
+        heightEt = (EditText) findViewById(R.id.heightEt);
+        birthdayEt = (EditText) findViewById(R.id.birthdayEt);
+
+        sexRg = (RadioGroup) findViewById(R.id.sexRG);
+
         reportRv = (RecyclerView) findViewById(R.id.reportRv);
         reportRv.setLayoutManager(new LinearLayoutManager(this));
 
         indicatorAdapter = new IndicatorAdapter(this);
         reportRv.setAdapter(indicatorAdapter);
+
+        findViewById(R.id.modifyBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modifyUserInfo();
+            }
+        });
 
         connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +121,53 @@ public class ConnectActivity extends AppCompatActivity implements QNBleCallback 
         connectBtn.setText("连接");
 
         doConnect();
+    }
+
+    void resetUserView() {
+        this.idEt.setText(this.user.getId());
+        this.sexRg.check(this.user.getGender() == 0 ? R.id.sexWoman : R.id.sexMan);
+        this.birthdayEt.setText(new SimpleDateFormat("yyyy-MM-dd").format(this.user.getBirthday()));
+        this.heightEt.setText(String.valueOf(this.user.getHeight()));
+    }
+
+    void modifyUserInfo() {
+        QNUser newUser = buildUser();
+        this.user = newUser;
+        bleApi.setUser(device.getMac(), newUser.getId(), newUser.getHeight(), newUser.getGender(), newUser.getBirthday());
+    }
+
+    QNUser buildUser() {
+        String id = idEt.getText().toString();
+        String errorString = null;
+        Date birthday = null;
+        if (id.trim().equals("")) {
+            errorString = "请填写有效的用户id";
+        } else if (heightEt.getText().length() == 0) {
+            errorString = "请填写有效的身高";
+        } else {
+            String birthdayString = birthdayEt.getText().toString();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-d");
+            try {
+                birthday = dateFormat.parse(birthdayString);
+            } catch (Exception e) {
+                errorString = "请按照 yyyy-M-d 的格式输入生日";
+            }
+        }
+
+        if (errorString != null) {
+            Toast.makeText(this, errorString, Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        int height = Integer.parseInt(heightEt.getText().toString());
+        int gender;
+        if (sexRg.getCheckedRadioButtonId() == R.id.sexMan) {
+            gender = 1;
+        } else {
+            gender = 0;
+        }
+
+        return new QNUser(id, height, gender, birthday);
+
     }
 
     void doConnect() {
@@ -148,7 +225,7 @@ public class ConnectActivity extends AppCompatActivity implements QNBleCallback 
 
     @Override
     public void onLowPower() {
-        Log.d("hdr", "电池电量低");
+        Log.d("hdr", "称端电量过低");
     }
 
 
